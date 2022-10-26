b3efe12dd7154bc44e0b26215d933e8cd39bec7b
songsangeon <thdtkddjs3@naver.com>
2022-10-26 11:23:23 +0900

    added

--- Step05_Constructor/src/test/main/MainClass04.java
+++ Step05_Constructor/src/test/main/MainClass04.java
@@ -15,7 +15,9 @@ public class MainClass04 {
 		System.out.println((int)c);
 		System.out.println(a.nextInt(100));
 		
+		//1~45사이의 랜덤한 정수를 얻는다
 		int d = a.nextInt(45) + 1;
+		//true false 중 하나의 값을 얻는다.
 		boolean e = a.nextBoolean();
 		
 		System.out.println(e);
