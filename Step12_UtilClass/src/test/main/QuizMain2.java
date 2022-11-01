99044e13622cb4dcdc5db6e2afdc1f61f48883b8
songsangeon <thdtkddjs3@naver.com>
2022-11-01 10:51:06 +0900

    Quiz main fixed and

--- Step12_UtilClass/src/test/main/QuizMain2.java
+++ Step12_UtilClass/src/test/main/QuizMain2.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class QuizMain {
+public class QuizMain2 {
 	public static void main(String[] args) {
 		//sample 데이터
 		Map<String, String> dic=new HashMap<>();
@@ -19,18 +19,25 @@ public class QuizMain {
 		 * 단어를 검색하여 그 뜻을 출력해보자
 		 */
 		Scanner scan=new Scanner(System.in);
-		//해당 키값이 존재하는지 여부를 알아낸다.
-		boolean isExist=dic.containsKey(scan);
 		
-		for(int i=0; i<3; i++) {
+		
+		
+		while(true) {
 			System.out.println("검색할 단어를 입력하세요:");
 			String msg=scan.nextLine();
-			System.out.println(msg);
+			System.out.println("입력한 메시지 :"+msg);
+			//해당 키값이 존재하는지 여부를 알아낸다.
+			boolean isExist=dic.containsKey(msg);
 			//get() 메소드 사용 시 key값이 없으면
 			//String의 경우 null이, 
 			//int의 경우 0이나 -1(이쪽이 더 많다)이 리턴된다.
 			String result = dic.getOrDefault(msg, "목록에 없습니다.");
-			if (result=="목록에 없습니다.") {
+			if (msg.equals("q")) {
+				System.out.println("검색을 종료합니다.");
+				break;
+			}
+			
+			if (!isExist) {
 				System.out.println(msg+" 의 뜻은 "+result);
 			}
 			else {System.out.println(msg+" 의 뜻은 "+result+"입니다.");}
